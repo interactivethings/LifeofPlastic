32924a2b8366326522e1ed0bf0e58161e582ac8a
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2020-08-06 10:54:13 -0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Josefin+Slab:wght@300;400&family=Muli:wght@200;300;400&display=swap"
             rel="stylesheet"
           />
+          <title>Life of Plastic</title>
+          <meta property="og:title" content="Life of Plastic" key="title" />
+          <meta name="description" content="A data visualization to explore the complex relationship of plastic and its recycling system." />
+          <meta property="og:description" content="A data visualization to explore the complex relationship of plastic and its recycling system." />
         </Head>
         <body>
           <Main />
